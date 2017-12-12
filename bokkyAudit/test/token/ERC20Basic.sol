425e174a22eb6058a078f1af9254877d233ba5a2
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2017-12-12 18:36:22 +1100

    Reverting accidental comments

--- bokkyAudit/test/token/ERC20Basic.sol
+++ bokkyAudit/test/token/ERC20Basic.sol
@@ -1,4 +1,3 @@
-// BK Ok
 pragma solidity ^0.4.18;
 
 
@@ -7,13 +6,9 @@ pragma solidity ^0.4.18;
  * @dev Simpler version of ERC20 interface
  * @dev see https://github.com/ethereum/EIPs/issues/179
  */
-// BK Ok
 contract ERC20Basic {
-  // BK Ok
   uint256 public totalSupply;
-  // BK Next 2 Ok
   function balanceOf(address who) public view returns (uint256);
   function transfer(address to, uint256 value) public returns (bool);
-  // BK Ok - Log event
   event Transfer(address indexed from, address indexed to, uint256 value);
 }
